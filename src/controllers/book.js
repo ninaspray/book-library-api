46d61e2dacf77815510e9668bbe531f607a9b393
ninaspray <ninaspray@gmail.com>
2021-04-05 14:43:52 +0100

    Books Helper controller updated

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,78 +1,88 @@
-const { Book } = require('../models');
-const book = require('../models/book');
+const {
+  getAllItems,
+  createItem,
+  updateItem,
+  getItemById,
+  deleteItem,
+} = require('./helpers');
 
-// const getBook = (_, res) => {
+// const getBooks = (_, res) => {
 //   Book.findAll().then(books => {
 //     res.status(200).json(books);
 //   });
 // }
+// const createBook = (req, res) => {
+//   const newBook = req.body;
+//   Book
+//     .create(newBook)
+//     .then(newBookCreated => res.status(201).json(newBookCreated));
+// };
+const getBooks = (_, res) => getAllItems(res, 'book');
 
-const getBooks = (_, res) => {
-  Book.findAll().then(books => {
-    res.status(200).json(books);
-  });
-}
+const createBook = (req, res) => createItem(res, 'book', req.body);
 
-const createBook = (req, res) => {
-  const newBook = req.body;
 
-  Book
-    .create(newBook)
-    .then(newBookCreated => res.status(201).json(newBookCreated));
-};
+// const updateBook = (req, res) => {
+//   const { id } = req.params;
+//   const newDetails = req.body;
 
-const updateBook = (req, res) => {
-  const { id } = req.params;
-  const newDetails = req.body;
+//   Book
+//     .update(newDetails, { where: { id } })
+//     .then(([recordsUpdated]) => {
+//       if (!recordsUpdated) {
+//         res.status(404).json({ error: 'The book could not be found.' });
+//     } else {
+//       Book.findByPk(id).then((updatedBook) => {
+//         res
+//         .status(200)
+//         .json(updatedBook);
+//     }
+//       )}
+//   });
+// }
 
-  Book
-    .update(newDetails, { where: { id } })
-    .then(([recordsUpdated]) => {
-      if (!recordsUpdated) {
-        res.status(404).json({ error: 'The book could not be found.' });
-    } else {
-      Book.findByPk(id).then((updatedBook) => {
-        res
-        .status(200)
-        .json(updatedBook);
-    }
-      )}
-  });
-}
+const updateBook = (req, res) =>
+  updateItem(res, 'book', req.body, req.params.id);
 
-const getBookById = (req, res) => {
-  const { id } = req.params;
 
-  Book.findByPk(id).then(book => {
-    if (!book) {
-      res
-        .status(404)
-        .json({ error: 'The book could not be found.' });
-    } else {
-      res
-        .status(200)
-        .json(book);
-    }
-  });
-}
 
-const deleteBook = (req, res) => {
-  const { id } = req.params;
+// const getBookById = (req, res) => {
+//   const { id } = req.params;
 
-  Book
-    .findByPk(id)
-    .then(foundBook => {
-      if (!foundBook) {
-        res.status(404).json({ error: 'The book could not be found.' });
-      } else {
-        Book
-          .destroy({ where: { id } })
-          .then(() => {
-            res.status(204).send();
-        });
-    }
-  });
-}
+//   Book.findByPk(id).then(book => {
+//     if (!book) {
+//       res
+//         .status(404)
+//         .json({ error: 'The book could not be found.' });
+//     } else {
+//       res
+//         .status(200)
+//         .json(book);
+//     }
+//   });
+// }
+const getBookById = (req, res) => getItemById(res, 'book', req.params.id);
+
+
+// const deleteBook = (req, res) => {
+//   const { id } = req.params;
+
+//   Book
+//     .findByPk(id)
+//     .then(foundBook => {
+//       if (!foundBook) {
+//         res.status(404).json({ error: 'The book could not be found.' });
+//       } else {
+//         Book
+//           .destroy({ where: { id } })
+//           .then(() => {
+//             res.status(204).send();
+//         });
+//     }
+//   });
+// }
+
+const deleteBook = (req, res) => deleteItem(res, 'book', req.params.id);
 
 module.exports = {
     getBooks,
